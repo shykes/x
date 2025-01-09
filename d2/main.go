fd4aa91c4711ab6587f983ea2334f37f2ac8f2c3
Solomon Hykes <solomon@dagger.io>
2025-01-09 11:12:41 -0800

    d2: preserve .svg extension on svg output files
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- d2/main.go
+++ d2/main.go
@@ -15,6 +15,6 @@ func (m *D2) Svg(source *dagger.File) *dagger.Directory {
 		WithWorkdir("/d2").
 		WithMountedFile("./source.d2", source).
 		WithWorkdir("./output").
-		WithExec([]string{"d2", "../source.d2", "."}).
+		WithExec([]string{"d2", "../source.d2", "..svg"}).
 		Directory(".")
 }
